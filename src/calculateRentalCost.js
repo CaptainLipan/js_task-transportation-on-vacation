736185ec5f7127660a0cfa94117f071bc11fa4d6
honzik <klablena.j@seznam.cz>
2025-03-02 15:55:57 +0100

    Solution edit

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,16 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const dailyRent = 40;
-  const finalPrice = days * dailyRent;
+  const DAILY_RENT = 40;
+  const DISCOUNT_3_TO_6_DAYS = 20;
+  const DISCOUNT_7_OR_MORE_DAYS = 50;
+  const DAYS_FOR_20_DISCOUNT = 3;
+  const DAYS_FOR_50_DISCOUNT = 7;
 
-  if (days < 3) {
-    return finalPrice;
-  } else if (days >= 3 && days < 7) {
-    return finalPrice - 20;
-  } else {
-    return finalPrice - 50;
+  const totalCost = days * DAILY_RENT;
+
+  if (days < DAYS_FOR_20_DISCOUNT) {
+    return totalCost;
   }
+
+  if (days < DAYS_FOR_50_DISCOUNT) {
+    return totalCost - DISCOUNT_3_TO_6_DAYS;
+  }
+
+  return totalCost - DISCOUNT_7_OR_MORE_DAYS;
 }
 
 module.exports = calculateRentalCost;
